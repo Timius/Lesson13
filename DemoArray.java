b4c2aa94a3ad2e3b1a0a9d420e29be8929340239
Timius <timius@bk.ru>
2016-11-26 20:51:45 +0800

    Initial commit

---
+++ DemoArray.java
@@ -0,0 +1,92 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by User on 23.11.2016.
+ */
+public class DemoArray {
+
+    private     int[] array;
+    private int length = 0;
+    public DemoArray () {    //что это
+        array = new int[10];
+    }
+
+    public void lenght (int i){
+        array = new int[i];
+    }
+
+   protected void creat() {
+
+        for (int i = 0; i < array.length; i++) {
+            int k = new Random().nextInt(50);
+            array[i] = k;
+        }
+
+        System.out.println("Исходный массив" + Arrays.toString(array));
+        }
+
+    protected int max(){
+        int max = array[0];
+        for (int i = 0; i < array.length; i++)
+            if (max < array[i]) {
+                max = array[i];
+                }
+        System.out.println("max=" + max);
+        return max;
+    }
+    protected int min(){
+        int min = array[0];
+        for (int i = 0; i < array.length; i++)
+            if (min > array[i]) {
+                min = array[i];
+            }
+        System.out.println("min=" + min);
+        return min;
+    }
+    protected double avg(){
+        double avg = 0;int n =0;
+        for (int i = 0; i < array.length; i++)
+            if (array[i]!=0) {
+                avg += array[i];
+                n++;
+            }
+        avg = avg/n;
+        System.out.println("avg=" + avg + "; n=" + n);
+        return avg;
+    }
+
+    protected void addElement (int j, int value){
+        int[] arrayN = new int[array.length+1];
+
+        for (int i = 0; i < array.length; i++){
+        arrayN[i]=array[i];}
+        for (int i = arrayN.length; i > j; i--) {
+            arrayN[i-1] = arrayN[i - 2];
+        }
+        arrayN[j]= value;
+        System.out.println("Увеличенный массив " + Arrays.toString(arrayN));
+    }
+
+    protected int [] deleteElement (int j) {
+        for (int i = j; i < array.length-1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        int[] arrayD = new int[array.length - 1];
+        for (int i = 0; i < arrayD.length; i++) {
+           arrayD[i] = array[i];
+        }
+        int [] array = arrayD; //сделал вариант с перезаписыванием исходного массива
+//        System.out.println("Уменьшенный массив " + Arrays.toString(arrayD));
+        System.out.println("Уменьшенный массив " + Arrays.toString(array));
+return array;
+
+    }
+
+
+
+}
+
+
---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * Created by User on 23.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        DemoArray array1 = new DemoArray();
+
+        array1.lenght(10);
+        array1.creat();
+        array1.max();
+        array1.min();
+        array1.avg();
+        array1.addElement(8, 10);//довавляет элемент номер j значением value
+        array1.deleteElement(3);
+
+
+
+
+    }
+}
